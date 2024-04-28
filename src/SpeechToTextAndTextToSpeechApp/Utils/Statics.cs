92f600ef649641a3219c67516a503c64358d3ab1
Sebastian Jensen <apps@tsjdev-apps.de>
2024-04-28 08:13:22 +0200

    Add Statics for Host, Mode and Voices.

---
+++ src/SpeechToTextAndTextToSpeechApp/Utils/Statics.cs
@@ -0,0 +1,17 @@
+﻿namespace SpeechToTextAndTextToSpeechApp.Utils;
+
+internal class Statics
+{
+    public const string AzureOpenAIKey = "Azure OpenAI";
+    public const string OpenAIKey = "OpenAI";
+
+    public const string TTSKey = "Text-to-Speech";
+    public const string STTKey = "Speech-To-Text";
+
+    public const string AlloyVoice = "alloy";
+    public const string EchoVoice = "echo";
+    public const string FableVoice = "fable";
+    public const string OnyxVoice = "onyx";
+    public const string NovaVoice = "nova";
+    public const string ShimmerVoice = "shimmer";
+}
